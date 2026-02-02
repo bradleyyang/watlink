6bd902b49d342fba7a394565c8800d4c72984571
bradleyyang <bradley.yang023@gmail.com>
2026-02-02 00:45:12 -0500

    testing middleware

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,23 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 
+const middleware = function (req, res, next) {
+    console.log('middleware function ran');
+    next();
+};
+
+app.use(middleware);
+
 app.get('/', (req, res) => {
     res.send('test success');
+});
+
+app.get('/home', (req, res) => {
+    res.json({ message: 'hello' });
 });
 
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
+
